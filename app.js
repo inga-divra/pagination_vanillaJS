df6ed8d47107a67670d7a8bdf422caf9e1f581bf
inga-divra <ingady@gmail.com>
2022-12-02 14:09:49 +0200

    fetchUsers js & display followers js

---
+++ app.js
@@ -0,0 +1,16 @@
+import fetchFollowers from './fetchFollowers.js'
+import displayFollowers from './displayFollowers.js'
+import paginate from './paginate.js'
+import displayButtons from './displayButtons.js'
+
+const title = document.querySelector('.section-title h1')
+
+const init = async () => {
+    const followers = await fetchFollowers();
+    title.textContent = 'pagination'
+    displayFollowers(followers);
+    const pages = paginate(followers);
+    console.log(pages);
+}
+
+window.addEventListener('load', init);
